84d695c1d224ad21b14d1d7538808a6420620cd7
Audrey Felicio Anwar <audreyanwar@gmail.com>
2021-06-14 05:19:48 +0800

    Implement log view UI

--- src/app/log-view/log-view.component.ts
+++ src/app/log-view/log-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoggingService } from '../core/services/logging.service';
 
 @Component({
   selector: 'app-log-view',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(public logger: LoggingService) { }
 
-  ngOnInit() {
-  }
+  ngOnInit(): void {}
 
 }
